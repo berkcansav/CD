8886a465a351c888d7a1c5d9c80f478823076374
berkcansav <berkcanuy@gmail.com>
2024-10-13 00:18:29 +0300

    Lab#1

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+// pages/lab1.js
+
+export default function Lab1() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <div className="bg-white rounded-lg shadow-lg p-8 max-w-md text-center">
+        <h1 className="text-3xl font-bold text-gray-900 mb-4">Laboratory Work #1</h1>
+        <p className="text-sm text-gray-400 mb-6">by Berk Can Yıldır KH-222ia.e</p>
+        <p className="text-xl font-medium text-gray-700">
+          Simple Next.js app for CI/CD Cloud Computing
+        </p>
+      </div>
+    </div>
+  );
+}
